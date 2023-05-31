bc7640bcb3f4b3d20869d88a37d23a66af75b57a
dyells07 <bhimkhanal999@gmail.com>
2023-05-31 21:34:56 +0545

    MVC database connection

---
+++ Product_Master/Product_Master/Controllers/ProductController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using Product_Master.Models;
+
+
+namespace Product_Master.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProductCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct(ProductModel product)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var dbconfig = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json").Build();
+
+                    string dbConnectionStr = "";
+                    if (!string.IsNullOrEmpty(dbconfig.ToString()))
+                    {
+                        dbConnectionStr = dbconfig["ConnectionStrings:DefaultConnection"];
+                        using (SqlConnection connection = new SqlConnection(dbConnectionStr))
+                        {
+                            string sql = "SP_Add_New_Product";
+                            using (SqlCommand cmd = new SqlCommand(sql, connection))
+                            {
+                                cmd.CommandType = CommandType.StoredProcedure;
+                                cmd.Parameters.AddWithValue("@ProductName", product.ProductName);
+                                cmd.Parameters.AddWithValue("@ProductDescription", product.ProductDescription);
+                                cmd.Parameters.AddWithValue("@ProductCost", product.ProductCost);
+                                cmd.Parameters.AddWithValue("@Stock", product.Stock);
+                                connection.Open();
+                                cmd.ExecuteNonQuery();
+                                connection.Close();
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return RedirectToAction("Product");
+        }
+    }
+}
---
+++ Product_Master/Product_Master/Models/ProductModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Product_Master.Models
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+        public string? ProductName { get; set; }
+        public string? ProductDescription { get; set;}
+        public decimal? ProductCost { get; set; }
+        public int Stock { get; set; }  
+    }
+}
